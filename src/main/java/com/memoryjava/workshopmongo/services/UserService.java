f71c07d6ed4f2773660864987849faab105e7e8b
Carlos Henrique <carloshrr1209@gmail.com>
2026-07-09 17:48:57 -0300

    Exception Handler Implementation

--- src/main/java/com/memoryjava/workshopmongo/services/UserService.java
+++ src/main/java/com/memoryjava/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.memoryjava.workshopmongo.services;
 
 import com.memoryjava.workshopmongo.domain.User;
 import com.memoryjava.workshopmongo.repository.UserRepository;
+import com.memoryjava.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,15 @@ public class UserService {
         return repository.findAll();
     }
 
+    public User findById(String id) {
+        User user = repository.findById(id).orElse(null);
+
+        if (user == null) {
+            throw new ObjectNotFoundException("Object not found.");
+        }
+
+        return user;
+    }
 
 
 }
